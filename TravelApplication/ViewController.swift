70352f604d2d49fcef064927e3ea4f001820e367
Povilas Staskus <povilas.staskus@gmail.com>
2018-09-26 14:30:09 +0300

    Initial Project Commit

---
+++ TravelApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelApplication
+//
+//  Created by Povilas Staskus on 9/26/18.
+//  Copyright © 2018 Povilas Staskus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
