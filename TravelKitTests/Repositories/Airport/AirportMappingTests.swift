11268d013504e512f39cd8e93d9b313d454e1412
Povilas Staskus <povilasstaskus@lith9175.lt.revelup.com>
2018-10-26 17:20:17 +0300

    Failing TravelKit tests

---
+++ TravelKitTests/Repositories/Airport/AirportMappingTests.swift
@@ -0,0 +1,26 @@
+//
+//  AirportMappingTests.swift
+//  TravelKitTests
+//
+//  Created by Povilas Staskus on 26/10/2018.
+//  Copyright © 2018 Povilas Staskus. All rights reserved.
+//
+
+import XCTest
+@testable import TravelKit
+
+class AirportMappingTests: XCTest {
+    func testSingleObjectMapping() {
+        // Given there is a valid airport JSON
+//        let json = AirportTest.ValidAiport.JSON
+//
+//        // When we map Airport
+//        let airport = Airport(JSON: json)!
+//
+//        // Then all properties should be mapped correctly
+//        let dictJson = json as NSDictionary
+//        let dictAirport = airport.toJSON() as NSDictionary
+//
+//        XCTAssertTrue(dictJson.isEqual(to: airport.toJSON()))
+    }
+}
