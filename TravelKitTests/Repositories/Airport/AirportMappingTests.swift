72804c58a56aae99da1e7f784de95535c66caa85
Povilas Staskus <povilas.staskus@gmail.com>
2018-10-28 17:22:38 +0200

    Expanded Airport Mapping tests

--- TravelKitTests/Repositories/Airport/AirportMappingTests.swift
+++ TravelKitTests/Repositories/Airport/AirportMappingTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import TravelKit
 
 class AirportMappingTests: XCTestCase {
-    func testSingleObjectMapping() {
+    func test_mapping_validJSON_shouldMap() {
         //Given there is a valid airport JSON
         let json = AirportTest.ValidAiport.JSON
 
@@ -19,5 +19,27 @@ class AirportMappingTests: XCTestCase {
 
         // Then all properties should be mapped correctly
         XCTAssertTrue((json as NSDictionary).isEqual(to: airport.toJSON()))
+    }
+    
+    func test_mapping_validJSON_withoutState_shouldMap() {
+        //Given there is a valid airport JSON without state
+        let json = AirportTest.ValidAiportWithoutState.JSON
+        
+        // When we map Airport
+        let airport = Airport(JSON: json)!
+        
+        // Then all properties should be mapped correctly
+        XCTAssertTrue((json as NSDictionary).isEqual(to: airport.toJSON()))
+    }
+    
+    func test_mapping_invalidJSON_shouldBeNil() {
+        //Given there is a valid airport JSON
+        let json = AirportTest.InvalidAiportWithoutCode.JSON
+        
+        // When we map Airport
+        let airport = try? Airport(JSON: json)
+        
+        // Then it should be nil
+        XCTAssertNil(airport)
     }
 }
